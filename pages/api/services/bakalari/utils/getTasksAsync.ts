eeae63c47ab1595879c61150c73f3f7db610c840
filipkastovsky <devfilipkastovsky@gmail.com>
2020-03-31 20:06:45 +0200

    feat(bakalari): getTasksAsync added; ITask added

---
+++ pages/api/services/bakalari/utils/getTasksAsync.ts
@@ -0,0 +1,34 @@
+import ITask from '../../../../../utils/ITask';
+import { Page } from 'puppeteer';
+import { KomensSelectors } from './Selectors';
+
+export default async (nOfMessages: number, page: Page) => {
+    const tasks: ITask[] = [];
+
+    for (let i = 1; i <= nOfMessages; i++) {
+        // .forEach doesn't support async
+        await page.click(`${KomensSelectors.messageListItem}:nth-child(${i})`); //! nth-child starts from 1
+
+        await page.waitFor(500); // View rerender can take up to 500ms at this point
+
+        const sender = await page.$eval(
+            KomensSelectors.senderDiv,
+            (el) => el.textContent,
+        );
+
+        const date = await page.$eval(
+            KomensSelectors.dateTd,
+            (el) => el.textContent,
+        );
+
+        const body = await page.$eval(
+            KomensSelectors.bodyDiv,
+            (el) =>
+                el.textContent!.replace(/\n|\t/g, '').replace(/\s{2,}/g, '\n'), //TODO: Source breaks line using HTML <br />, implement custom html to text converter
+        );
+
+        tasks.push({ sender, date, body });
+    }
+
+    return tasks;
+};
