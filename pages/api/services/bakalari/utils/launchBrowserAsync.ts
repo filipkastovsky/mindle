729b1f2de843878e9374626a69286f47fd53ee19
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-01 16:31:03 +0200

    fix: Include aws-lambda compatible browser with puppeteer

--- pages/api/services/bakalari/utils/launchBrowserAsync.ts
+++ pages/api/services/bakalari/utils/launchBrowserAsync.ts
@@ -1,9 +1,30 @@
-import puppeteer from 'puppeteer';
+// ! puppeteer is a dev dependency, will not be bundled in production (aws-lambda environment)
+// * Fallback to chrome-aws-lambda if puppeteer cannot be found
+// Side effect: If fallback fails, launchBrowserAsync will throw an error
 
 export default async () => {
-    const browser = await puppeteer.launch({
-        defaultViewport: { width: 1280, height: 720 },
-    });
+    const defaultViewport = { width: 1280, height: 720 };
+    let browser;
+
+    try {
+        // Development
+        const puppeteer = await require('puppeteer');
+        browser = await puppeteer.launch({
+            defaultViewport,
+        });
+    } catch {
+        // Production
+        const puppeteer = await require('puppeteer-core');
+        const chrome = await require('chrome-aws-lambda');
+
+        browser = await puppeteer.launch({
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+            defaultViewport,
+        });
+    }
+
     const page = await browser.newPage();
     return { browser, page };
 };
