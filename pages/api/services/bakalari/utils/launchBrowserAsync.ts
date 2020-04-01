8b18ff0d9ef1cd4cf302f910f45141c4be3c9873
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-01 17:31:36 +0200

    chore: Remove aws-lambda support

--- pages/api/services/bakalari/utils/launchBrowserAsync.ts
+++ pages/api/services/bakalari/utils/launchBrowserAsync.ts
@@ -1,29 +1,10 @@
-// * Fallback to chrome-aws-lambda if puppeteer cannot be found (AWS lambda limits package size to ~50MB)
-// Side effect: If fallback fails, launchBrowserAsync will throw an error
+import puppeteer from 'puppeteer';
 
 export default async () => {
-    const defaultViewport = { width: 1280, height: 720 };
-    let browser;
-
-    try {
-        const puppeteer = await require('puppeteer');
-        browser = await puppeteer.launch({
-            args: ['--no-sandbox'],
-            defaultViewport,
-        });
-    } catch {
-        // Fallback for AWS Lambda
-        const puppeteer = await require('puppeteer-core');
-        const chrome = await require('chrome-aws-lambda');
-
-        browser = await puppeteer.launch({
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
-            defaultViewport,
-        });
-    }
-
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        defaultViewport: { width: 1280, height: 720 },
+    });
     const page = await browser.newPage();
     return { browser, page };
 };
