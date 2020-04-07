c3d570fc49086026d3549b9347b2cd8e7b9928da
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-07 13:05:57 +0200

    feat(client): Display WIP page as homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import WorkInProgress from '../components/WorkInProgress';
 
 const IndexPage = () => (
-    <Layout>
-        <h1>Hello Next.js</h1>
+    <Layout title="Mindle">
+        <WorkInProgress />
     </Layout>
 );
 
