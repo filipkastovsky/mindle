882b2307ff3d2a2e7ba57bd978242c58dc48ad8d
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-11 16:24:06 +0200

    feat(frontend): Additional colors added

--- theme/Colors.ts
+++ theme/Colors.ts
@@ -4,6 +4,8 @@ enum Colors {
     Dark = '#3A3A3A',
     Light = '#F9F9F9',
     Grey = '#EFEFEF',
+    White = '#FFFFFF',
+    Gradient = 'linear-gradient(98.22deg, #43cfd9 0%, #74d14c 100%)',
 }
 
 export default Colors;
