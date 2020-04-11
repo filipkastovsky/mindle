8f540dc61fbf5c333680abcb4eaadd738f61a29e
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-11 17:53:07 +0200

    feat(frontend): Header component added

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ExitToApp } from '@material-ui/icons';
+import Colors from '../theme/Colors';
+
+interface IHeaderProps {
+    signedIn: boolean;
+    onSignOut: () => void;
+}
+
+const HeaderContainer = styled.div`
+    height: 4em;
+    width: 95%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Header: React.FC<IHeaderProps> = ({ signedIn, onSignOut }) => {
+    return (
+        <HeaderContainer>
+            <img src="/logos/mindle-logo.svg" alt="Mindle logo" />
+            {signedIn && (
+                <ExitToApp
+                    htmlColor={Colors.Dark}
+                    fontSize="large"
+                    onClick={onSignOut}
+                />
+            )}
+        </HeaderContainer>
+    );
+};
+
+export default Header;
