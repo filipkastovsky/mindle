acab19a1b8eb86d099f7c12becaa0af9a5a97db2
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-14 20:18:20 +0200

    feat(frontend): Deemphesized Paper variant added

--- components/Paper.tsx
+++ components/Paper.tsx
@@ -7,17 +7,21 @@ import styled from 'styled-components';
 import Colors from '../theme/Colors';
 
 export interface IPaperProps extends MuiPaperProps {
-    highlight?: boolean;
+    highlighted?: boolean;
+    deemphesized?: boolean;
 }
 
-const Paper = styled(({ highlight, ...props }: IPaperProps) => (
+const Paper = styled(({ highlighted, deemphesized, ...props }: IPaperProps) => (
     <MuiPaper {...props} />
 ))<IPaperProps>`
+    position: relative;
     display: flex;
     width: 100%;
     min-height: 3em;
-    background: ${({ highlight }) =>
-        highlight ? Colors.Gradient : Colors.Grey};
+    background: ${({ highlighted }) =>
+        highlighted ? Colors.Gradient : Colors.Grey};
+    opacity: ${({ deemphesized }) => (deemphesized ? '0.5' : '1')};
+    color: ${({ highlighted }) => (highlighted ? Colors.White : Colors.Dark)};
 `;
 
 export default Paper;
