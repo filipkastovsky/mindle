b4e928f504172a33e040797b5fb1133646909add
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-14 15:44:10 +0200

    feat(frontend): PaperWithSwitch component added

---
+++ components/PaperWithSwitch.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Paper, { IPaperProps } from './Paper';
+import Switch, { ISwitchProps } from './Switch';
+import styled from 'styled-components';
+
+export interface IPaperWithSwitchProps {
+    PaperProps?: IPaperProps;
+    SwitchProps: ISwitchProps;
+}
+
+const StyledPaper = styled(Paper)<IPaperProps>`
+    justify-content: space-between;
+    align-items: center;
+    font-weight: bold;
+    font-size: 1.17em;
+    padding-left: 1em;
+`;
+
+const PaperWithSwitch: React.FC<IPaperWithSwitchProps> = ({
+    PaperProps,
+    SwitchProps,
+    children,
+}) => {
+    return (
+        <StyledPaper {...PaperProps}>
+            <div>{children}</div>
+            <Switch {...SwitchProps} />
+        </StyledPaper>
+    );
+};
+
+export default PaperWithSwitch;
