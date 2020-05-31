ab5a497230aa7887273a31b3c4aca7f7e8e1f5c4
filipkastovsky <devfilipkastovsky@gmail.com>
2020-05-31 13:35:25 +0200

    fix(auth): Only refresh expired tokens

--- graphql/utils/isJwtExpired.ts
+++ graphql/utils/isJwtExpired.ts
@@ -1,4 +1,4 @@
 import jwtDecode from 'jwt-decode';
 import { IDecodedJwt } from '../../interfaces/IDecodedJwt';
 export const isJwtExpired = (jwt: string): boolean =>
-    (jwtDecode(jwt) as IDecodedJwt).exp < Date.now() / 1000;
+    (jwtDecode(jwt) as IDecodedJwt).exp > Date.now() / 1000;
