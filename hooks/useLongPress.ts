30ea32cb5eecaac00562108ff2c5380fbab77746
filipkastovsky <devfilipkastovsky@gmail.com>
2020-07-05 01:16:05 +0200

    fix(frontend): Prevent default

--- hooks/useLongPress.ts
+++ hooks/useLongPress.ts
@@ -3,11 +3,16 @@ import { useRef, useCallback } from 'react';
 export const useLongPress = (onLongPress?: Function, timeout = 500) => {
     const timeoutId = useRef<number>();
 
-    const handleTouchStart = useCallback(() => {
-        timeoutId.current = setTimeout(onLongPress, timeout);
-    }, [onLongPress, timeout]);
+    const handleTouchStart = useCallback(
+        (e) => {
+            e && e.preventDefault();
+            timeoutId.current = setTimeout(onLongPress, timeout);
+        },
+        [onLongPress, timeout],
+    );
 
-    const handleTouchEnd = useCallback(() => {
+    const handleTouchEnd = useCallback((e) => {
+        e && e.preventDefault();
         clearTimeout(timeoutId.current);
     }, []);
 
